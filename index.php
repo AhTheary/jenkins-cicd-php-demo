728ea9d08575e0c327d2de3ffd9eb87874f76667
KHEANG Sokuntheary <86469224+AhTheary@users.noreply.github.com>
2022-08-30 09:58:17 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>Index</title>
  </head>
  <body>
- <?php echo '<p>Hello world from PHP! Declara!hhhhhhhhhhhhhhh</p>'; ?> 
+ <?php echo '<p>Hello world from PHP! Declara!hhhhhhhhhhhhh</p>'; ?> 
  </body>
 </html>
