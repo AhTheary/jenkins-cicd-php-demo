33b2387cfadcbf7c6d5d5de8f6ba86b60e7e4281
KHEANG Sokuntheary <86469224+AhTheary@users.noreply.github.com>
2022-09-07 16:45:24 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,23 @@
-<html>
- <head>
-  <title>Index</title>
- </head>
- <body>
- <?php echo '<p>Hello world from PHP! !!</p>'?> 
- </body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP-02</title>
+</head>
+<body>
+    <?php
+        $like = "I like php";
+        $num = 7;
+        echo $like . $num;
+        
+        echo "<p>";
+        echo $like . " you " . $num;
+        echo"</p>";
+
+        echo "My favorite php version is $num $like";
+    ?>
+
+</body>
 </html>
