e16ec37434a75bb2914ef27aa5145137462319cc
KHEANG Sokuntheary <86469224+AhTheary@users.noreply.github.com>
2022-09-07 16:46:28 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,20 +4,39 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP-02</title>
+    <title>PHP-05</title>
 </head>
 <body>
     <?php
-        $like = "I like php";
-        $num = 7;
-        echo $like . $num;
-        
-        echo "<p>";
-        echo $like . " you " . $num;
-        echo"</p>";
+    $i = 1;
 
-        echo "My favorite php version is $num $like";
+    while ($i <= 5){
+        echo"Hello while $i times"."<br>";
+        $i++;
+    }
     ?>
+    <?php
+    $i = 0;
 
+   do{
+       $i++;
+       echo"php do ...while loop $i times"."<br>";
+    }while($i <=5);
+    ?>
+    <?php
+    
+    for ($i=1; $i<=5 ; $i++) { 
+        echo"PHP for loop print $i times.<br>";
+    }
+    ?>
+    <?php
+    $salary[]=12000;
+    $salary[] = 3000;
+    $salary[]=5000;
+
+    foreach($salary as $value){
+        echo"Salary: $value<br>";
+    }
+    ?>
 </body>
 </html>
