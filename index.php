9804285bc039952111647848fd233071290d32e5
KHEANG Sokuntheary <86469224+AhTheary@users.noreply.github.com>
2022-09-07 16:48:46 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,39 +4,25 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP-05</title>
+    <title>PHP-03</title>
 </head>
 <body>
     <?php
-    $i = 1;
-
-    while ($i <= 5){
-        echo"Hello while $i times"."<br>";
-        $i++;
-    }
+        $var = 1;
+        echo ++$var;
+        echo $var++;
+        echo --$var;
+        echo $var--;
+        echo "<br/>";
     ?>
-    <?php
-    $i = 0;
-
-   do{
-       $i++;
-       echo"php do ...while loop $i times"."<br>";
-    }while($i <=5);
-    ?>
-    <?php
     
-    for ($i=1; $i<=5 ; $i++) { 
-        echo"PHP for loop print $i times.<br>";
-    }
-    ?>
     <?php
-    $salary[]=12000;
-    $salary[] = 3000;
-    $salary[]=5000;
-
-    foreach($salary as $value){
-        echo"Salary: $value<br>";
-    }
+        $var = "value";
+        echo $var;
+        $var = 1;
+        echo ++$var;
+        $var += 3;
+        echo $var;
     ?>
 </body>
 </html>
